812c7d4b562d52c8ff778868baa688a96eb3c164
VeigaTorres <91731160+VeigaTorres@users.noreply.github.com>
2024-04-23 17:46:54 +0100

    initial HashTableSet

---
+++ src/main/kotlin/week11HashTable/HashTableSet.kt
@@ -0,0 +1,60 @@
+package week11HashTable
+
+class HashTableSet<K> ( capacityInitial:Int=5,
+                        val loadFactor: Float= 0.75f) {//: MutableSet<K> {
+    private class Node<K> (  val key: K,
+                             var next: Node<K>?,
+                             val hc: Int )
+    private var table: Array< Node<K>? > = arrayOfNulls(capacityInitial)
+    private var count = 0
+    private var threshold =  (capacityInitial * loadFactor).toInt()
+
+    val capacity: Int get() = table.size
+    val size: Int get() = count
+
+    private fun hash( hc: Int ): Int = hc.and( 0x7fff_ffff ) % capacity
+
+    private fun getNode( key: K ): Node<K>? {
+        TODO()
+    }
+
+    fun contains(element: K): Boolean {
+        TODO()
+    }
+
+    fun add(element: K) : Boolean {
+        TODO()
+        ++count
+        if( count > threshold  ) expand()
+        return true
+    }
+
+    fun remove(element:K) : Boolean { //O(1)
+        TODO()
+     }
+
+    private fun expand() {
+       val oldTable = table
+       table = arrayOfNulls(capacity * 2 )
+       threshold = (capacity * loadFactor).toInt()
+       TODO()
+     }
+
+    fun iterator() =
+        object : MutableIterator<K> {
+
+            override fun hasNext(): Boolean {
+                TODO()
+            }
+
+            override fun next(): K {
+                TODO()
+            }
+
+            override fun remove() {
+                TODO()
+            }
+
+        }
+
+}
---
+++ src/main/kotlin/week11HashTable/colUtils.kt
@@ -0,0 +1,49 @@
+package week11HashTable
+
+import week9Collection.LinkedCollection
+import java.util.TreeSet
+
+fun listTopTen(values: Array<Int> ) {
+    val topTen = TreeSet<Int>();
+    for( v in values ) {
+        if( topTen.size < 10 ) topTen.add(v)
+        else {
+            val less= topTen.first()
+            if ( less < v ) {
+                topTen.remove( less )
+                topTen.add( v )
+            }
+        } }
+    topTen.forEach{ println(it) }
+}
+
+fun wordsThatOcurrMost( list: Collection<String> ):Set<String> {
+    val words= HashMap<String, Int>()
+    // Contar o número de ocorrências de cada palavra.
+    for ( w in list ) {
+        var c = words.get( w ) // (1)
+        if ( c == null ) c = 0 // (2)
+        // (1) e (2) podem ser substituidas por: val c = words.getOrDefault(w, 0);
+        words.put( w, c + 1 )
+    }
+    if ( words.isEmpty() ) return emptySet()
+    //Obter a vista correspondente à coleção de contadores.
+    val counts = words.values
+    val maxOcurr = counts.max()
+    // Remover do CONTENTOR ASSOCIATIVO, as palavras que não têm
+    // o máximo de ocorrências.
+    counts.retainAll( listOf( maxOcurr ) );
+    return words.keys;
+}
+
+fun partition( c: Collection<Int>,
+               pred: (Int)-> Boolean): Collection<Int> {
+    val result = LinkedCollection<Int>();
+    for( element in c )
+        if ( pred(element) )
+            result.addFirst(element)
+        else
+            result.addLast( element );
+    return result;
+}
+
